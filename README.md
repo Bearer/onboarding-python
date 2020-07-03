0858ae3c3a7c44d37ef28e5bc2e5dd019e2b13ad
Cédric Fabianski <cfabianski@me.com>
2020-07-03 17:21:24 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Bearer Onboarding Python
+
+This sample application allows you to try out [Bearer](https://bearer.sh) before installing it in your application. To get started, you will need:
+
+- An account on Bearer ([Sign up here](http://app.bearer.sh/signup))
+- Your Bearer App Key, found [here in the settings](https://app.bearer.sh/settings/general).
+- Python 3.7+
+
+## Installation
+
+First, clone this repo.
+
+```shell
+# Clone the app
+git clone https://github.com/Bearer/onboarding-python.git
+```
+
+Next, navigate to the newly created directory.
+
+```shell
+# Change directory
+cd onboarding-python
+```
+
+Finally, run `pip` to install the dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running the application
+
+To start the application, run the following command and replace the `XXXX` with your Bearer App Key.
+
+```bash
+BEARER_SECRET_KEY='XXXX' python demo.py
+```
+
+## Viewing the results
+
+Once the application has finished running, you can view the results by visiting the [dashboard](https://app.bearer.sh).
+
+To get started with Bearer in your own application, check our or [Ruby Agent Documentation](https://ruby.docs.bearer.sh/).
+
+If you need any help along the way, you can get support at our [Help Center](https://support.bearer.sh) or by clicking the Intercom button directly in the dashboard.
---
+++ init.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+import bearer_agent
+
+import logging
+logging.basicConfig()
+logging.getLogger("bearer").setLevel(logging.DEBUG)
+
+import httpx
+
+print(f"Agent version {bearer_agent.__version__}")
+
+bearer_agent.init(
+  secret_key=os.environ.get("BEARER_SECRET_KEY"),
+  environment=os.environ.get("APP_ENV")
+)
+
+# Postman-echo
+print("-- Sending API calls to Postman-Echo --")
+httpx.get("https://postman-echo.com/status/404",
+              headers={"password": "h4x0r", "secret": "secret key"})
+
+httpx.get("https://postman-echo.com/status/403",
+              headers={"password": "h4x0r", "secret": "secret key"})
+
+
+httpx.get("https://postman-echo.com/status/429",
+              headers={"password": "h4x0r", "secret": "secret key"})
+
+httpx.get("https://postman-echo.com/status/501?email=pii@example.com",
+              headers={"password": "h4x0r", "secret": "secret key"})
+
+httpx.get("https://postman-echo.com/status/200")
+
+httpx.post("https://postman-echo.com/post",
+              headers={"Content-Type": "application/json"},
+              data={"foo": "Bar"})
+
+# NASA API
+print("-- Sending API calls to NASA API --")
+httpx.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+# Star Wars API
+print("-- Sending API calls to SWAPI --")
+httpx.get("https://swapi.dev/api/people/1/")
+httpx.get("https://swapi.dev/api/people/9/")
+httpx.get("https://swapi.dev/api/starships/9/")
+
+# Foo Bar -> Non Existing API
+print("-- Sending API calls to non existing API --")
+try:
+  httpx.get("https://foo.bar/status/200")
+except:
+  print("")
